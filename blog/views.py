eb4757df29249d7b8affc503022fad9ee6e8dab1
RatGick <fachion.xxx@gmail.com>
2019-05-05 22:56:57 +0200

    Brand-new main blog page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
+
+
+def motherblog(request):
+    posts = Blog.objects
+    return render(request, 'blog/motherblog.html', {'posts': posts})
